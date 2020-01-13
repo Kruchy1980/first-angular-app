f2194b0ba61a1913a11335392153576ab86f3f9e
Kruchy1980 <jakub.kruszynski@vp.pl>
2020-01-13 15:22:59 +0100

    Repo1 ngModule - excercise

--- angular-hello-world/src/app/app.module.ts
+++ angular-hello-world/src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { UserItemComponent } from './user-item/user-item.component';
-import { UserListComponent } from './user-list/user-list.component';
+import { AppComponent } from './app.component'
+import { HelloWorldComponent } from './hello-world/hello-world.component'
+import { UserItemComponent } from './user-item/user-item.component'
+import { UserListComponent } from './user-list/user-list.component'
 
 @NgModule({
   declarations: [
@@ -13,10 +13,8 @@ import { UserListComponent } from './user-list/user-list.component';
     UserItemComponent,
     UserListComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
